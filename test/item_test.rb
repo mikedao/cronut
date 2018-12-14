365f90f27599db74096021c9bec44277a8c8ebc7
TimothyFell <timothy.fell846@gmail.com>
2018-12-14 10:59:21 -0700

    added batter unit tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -9,7 +9,7 @@ require './lib/item_things.rb'
 class StoreTest<Minitest::Test
 
   def setup
-    @item_data = {
+    item_data = {
       "id": "0001",
       "type": "donut",
       "name": "Cake",
@@ -28,36 +28,30 @@ class StoreTest<Minitest::Test
           { "id": "5006", "type": "Chocolate with Sprinkles" },
           { "id": "5003", "type": "Chocolate" },
           { "id": "5004", "type": "Maple" }]}
+    @item = Item.new(item_data)
   end
 
   def test_it_exists
-    item = Item.new(@item_data)
-
-    assert_instance_of Item, item
+    assert_instance_of Item, @item
   end
 
   def test_it_has_basic_attributes
-    item = Item.new(@item_data)
-
-    assert_equal "0001", item.id
-    assert_equal "donut", item.type
-    assert_equal "Cake", item.name
-    assert_equal 0.55, item.ppu
-
+    assert_equal "0001", @item.id
+    assert_equal "donut", @item.type
+    assert_equal "Cake", @item.name
+    assert_equal 0.55, @item.ppu
   end
 
   def test_it_generates_batters
-    item = Item.new(@item_data)
-    assert_equal 4, item.batter.count
-    assert_equal "1001", item.batter.first.id
-    assert_equal "Regular", item.batter.first.type
+    assert_equal 4, @item.batter.count
+    assert_equal "1001", @item.batter.first.id
+    assert_equal "Regular", @item.batter.first.type
   end
 
-  def test_it_generates_topping
-    item = Item.new(@item_data)
-    assert_equal 7, item.toppings.count
-    assert_equal "5001", item.toppings.first.id
-    assert_equal "None", item.toppings.first.type
+  def test_it_generates_toppings
+    assert_equal 7, @item.toppings.count
+    assert_equal "5001", @item.toppings.first.id
+    assert_equal "None", @item.toppings.first.type
   end
 
 end
